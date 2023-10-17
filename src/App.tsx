ee155393caa6212aa965f12e867470d269001f2f
Taalaibek Sagynaliev <sagynalievv.t@gmail.com>
2023-10-17 16:42:09 +0600

    Add some new function in Circle

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import './App.css'
-
+import Circle from "./Circle/Circle";
 
 function App() {
     return (
         <div className="App">
-
+            <div>
+                <button>New numbers</button>
+            </div>
+            <Circle number={5}/>
+            <Circle number={1}/>
+            <Circle number={16}/>
+            <Circle number={23}/>
+            <Circle number={32}/>
         </div>
     )
 }
--- src/Circle/Circle.tsx
+++ src/Circle/Circle.tsx
@@ -1,1 +1,15 @@
+import React from "react";
 
+interface Props {
+    number: number;
+}
+
+const Circle: React.FC<Props> = ({number}) => {
+    return (
+        <div className="circle">
+            <span>{number}</span>
+        </div>
+    )
+};
+
+export default Circle;
